c7ef6e72527e943705aa48399be1a03dbc46d0b8
QIN2DIM <62018067+QIN2DIM@users.noreply.github.com>
2021-08-18 00:52:28 +0800

    v2.0.3.beta.0
    修复站点链接变量的索引bug

--- examples/__init__.py
+++ examples/__init__.py
@@ -4,14 +4,15 @@
 # Github     : https://github.com/QIN2DIM
 # Description:
 
-from .demo_geetest2 import demo_geetest2, URLS_GT2
-from .demo_geetest3 import demo_geetest3, URLS_GT3
+from .demo_geetest2 import demo_geetest2
+from .demo_geetest3 import demo_geetest3
+from .demo_traditional import demo_traditional
 
 __version__ = ['0.1.1']
 
 __all__ = [
-    # 使用了对应验证版本的测试站点
-    'URLS_GT3', 'URLS_GT2',
     # demo示例
-    'demo_geetest2', 'demo_geetest3'
+    'demo_geetest2', 'demo_geetest3',
+    # 传统做法，参考对象
+    'demo_traditional',
 ]
